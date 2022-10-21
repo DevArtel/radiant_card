257723c616d1df479fbb893a97fc7fa14ca1dfa0
cupofme <danil.ilyashuk@gmail.com>
2022-10-21 18:02:22 +0200

    Implement transform amination

--- lib/src/glorious_card.dart
+++ lib/src/glorious_card.dart
@@ -22,52 +22,55 @@ class GloriousCard extends StatefulWidget {
   State<GloriousCard> createState() => _GloriousCardState();
 }
 
-class _GloriousCardState extends State<GloriousCard> {
-  Offset _localPosition = Offset.zero;
+class _GloriousCardState extends State<GloriousCard> with SingleTickerProviderStateMixin {
+  /// Pointer position of Offset(0, 0) is the center of the card
+  final _pointerPositionNotifier = ValueNotifier(Offset.zero);
+  var _desiredPointerPosition = Offset.zero;
+
+  @override
+  void initState() {
+    super.initState();
+
+    createTicker((elapsed) {
+      final difference = _pointerPositionNotifier.value - _desiredPointerPosition;
+
+      if (difference.distance < 1.0) {
+        return;
+      }
+
+      _pointerPositionNotifier.value = Offset.lerp(_pointerPositionNotifier.value, _desiredPointerPosition, 0.05)!;
+    }).start();
+  }
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
         return MouseRegion(
-          onHover: _onHover,
-          onExit: _onExit,
-          child: Transform(
-            transform: _getTransform(constraints),
-            alignment: FractionalOffset.center,
+          onHover: (event) => _desiredPointerPosition =
+              event.localPosition.translate(-constraints.maxWidth / 2, -constraints.maxHeight / 2),
+          onExit: (_) => _desiredPointerPosition = Offset.zero,
+          child: AnimatedBuilder(
+            animation: _pointerPositionNotifier,
             child: widget.child,
+            builder: (context, child) => Transform(
+              transform: _getTransform(constraints, _pointerPositionNotifier.value),
+              alignment: FractionalOffset.center,
+              child: child,
+            ),
           ),
         );
       },
     );
   }
 
-  Matrix4 _getTransform(BoxConstraints constraints) {
-    if (_localPosition == Offset.zero) {
-      return Matrix4.identity();
-    }
-
-    final center = Offset(
-      constraints.maxWidth / 2,
-      constraints.maxHeight / 2,
-    );
-
-    final angleX = (center.dy - _localPosition.dy) / center.dy * widget.maxAngle;
-    final angleY = (center.dx - _localPosition.dx) / center.dx * widget.maxAngle;
+  Matrix4 _getTransform(BoxConstraints constraints, Offset pointerPosition) {
+    final angleX = pointerPosition.dy / (constraints.maxHeight / 2) * widget.maxAngle;
+    final angleY = pointerPosition.dx / (constraints.maxWidth / 2) * widget.maxAngle;
 
     return Matrix4.identity()
       ..setEntry(3, 2, 0.001)
-      ..rotateX(angleX)
-      ..rotateY(-angleY);
-  }
-
-  void _onHover(PointerEvent event) {
-    setState(() {
-      _localPosition = event.localPosition;
-    });
-  }
-
-  void _onExit(PointerEvent event) {
-    setState(() => _localPosition = Offset.zero);
+      ..rotateX(-angleX)
+      ..rotateY(angleY);
   }
 }
